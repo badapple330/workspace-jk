c9ee7d244e017e661d6f2c8cbaf2a7adda36daf8
junpeikoizumi <badapple330@gmail.com>
2017-10-19 15:55:48 +0900

    test

---
+++ struts2/src/com/internousdev/struts2/action/GoCreditCardAction.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package com.internousdev.struts2.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author internousdev
+ *
+ */
+public class GoCreditCardAction extends ActionSupport{
+	public String execute(){
+		return SUCCESS;
+	}
+}
